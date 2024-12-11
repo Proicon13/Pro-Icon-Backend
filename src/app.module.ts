b6264af6ce95f0658f142484502f07df4d7d2b43
Ibrahim Ismail <51814249+Ibrahim-fci@users.noreply.github.com>
2024-12-11 23:54:09 +0200

    remove unneded_files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
-import { JwtAuthModule } from './jwt/jwt.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, JwtAuthModule, AuthModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,13 +3,8 @@
 import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateUserDto } from '../dto/createUser.dto';
 import { UpdateUserDto } from '../dto/updateUser.dto';
-import { LoginUserDto } from 'src/dto/login.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
-import { UserResponseDto } from 'src/swagger/respnse/user/createUser.dto';
-import { LoginUserResponseDto } from 'src/swagger/respnse/user/loginUser.dto';
-import { JwtAuthGuard } from 'src/guards/JwtAuthGuard';
+
 
 @Controller('users')
 export class UserController {
