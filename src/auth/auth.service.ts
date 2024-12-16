d7642616cc8c5d4e58dc6a717f001fe9ac87b526
Ibrahim Ismail <51814249+Ibrahim-fci@users.noreply.github.com>
2024-12-16 17:37:01 +0200

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -105,7 +105,7 @@ export class AuthService {
       };
     }
 
-    return new BadRequestException("Invalid credentials");
+    throw  new BadRequestException("Invalid credentials");
   }
 
   async sendResetPasswordEmail(sendResetEmailDto: SendResetEmailDto) {
